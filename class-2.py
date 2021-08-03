2542b8039c1ed749341cf470df3815db37a92a46
bishalbar77 <bishalbar77@users.noreply.github.com>
2021-08-03 18:28:21 +0530

    2nd class code update

---
+++ class-2.py
@@ -0,0 +1,27 @@
+# Different types of variables
+
+marks = 360             #  Integer Variable   
+flag = True             #  Boolean Variable
+name = "Rohan"          #  String Variable
+unitTestMarks = 9.5     # Float Variable
+students = ["Rohan", "Subham" , "Aditya", 'John', "Rutuja" ] # List Variable
+
+# Print Varibles in Python
+print(marks/12)
+print(students)
+
+# Take input from user in Python
+userName = input("Enter your name: ")
+print("My name is " + userName)
+
+# Take input in integer and find the multiplication result of that number
+number_1 = int(input("First Number: "))
+number_2 = int(input("Second Number: "))
+multiply = number_1 * number_2
+print("Multiple of two number is " + str(multiply))
+
+# Working with conditional statements 
+if(flag):
+    print("1st Statement")
+else:
+    print("2nd Statement")
