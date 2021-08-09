b89d9367d0c35feb231063bfee99e79a67e2feb6
bishalbar77 <bishalbar77@users.noreply.github.com>
2021-08-09 18:14:33 +0530

    7th class notes update

---
+++ tic-tac-toe.py
@@ -0,0 +1,58 @@
+'''
+Steps followed:
+1. Board Design
+2. Update board (User input on board)
+3. Play game
+4. Handle player (switch between X and O)
+5. Check if game is over - Who wins
+    - check for rows
+    - check for column
+    - check for diagonal
+6. Check for Tie
+7. Flip Player
+8. Back to 2
+
+Starting point computer case => 0
+Instead of 1
+'''
+
+# students =  ["Rohan", "Subham" , "Aditya", 'John', "Rutuja" ] # List Variable
+# students[2] = "Manoj"
+# print(students)
+
+board = [
+        "-", "-", "-",
+        "-", "-", "-",
+        "-", "-", "-"
+        ]
+# userInput = int(input())
+# board[userInput - 1] = "X"
+
+has_won = None
+
+def display_board():
+    print("\n")
+    print(board[0] + " | " + board[1] + " | " + board[2] + "    1 | 2 | 3")
+    print(board[3] + " | " + board[4] + " | " + board[5] + "    4 | 5 | 6")
+    print(board[6] + " | " + board[7] + " | " + board[8] + "    7 | 8 | 9")
+    print("\n")
+
+def handle_player():
+    return
+
+def check_if_game_over():
+    return
+
+def flip_player():
+    return
+
+def play_game():
+    display_board()
+    handle_player()
+    check_if_game_over()
+    flip_player()
+
+    if has_won == "X" or has_won == "O":
+        print("Our winner is "+has_won)
+    elif has_won == None:
+        print("Game Tie")
