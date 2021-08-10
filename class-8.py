b9f0fb475b97390b5988432ff8a02d99dbcef622
bishalbar77 <bishalbar77@users.noreply.github.com>
2021-08-10 18:14:15 +0530

    8th class notes update

---
+++ class-8.py
@@ -0,0 +1,15 @@
+# What is type casting?
+# Type Casting - Converting one data type into another
+# How to import other package or file
+import somePackage
+somePackage.pattern1()
+
+name = "Rutuja"
+
+# How to use global varible
+def pattern2(): 
+    global name
+    name = "Subham"
+
+pattern2()
+print(name)
--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -15,22 +15,29 @@ Steps followed:
 Starting point computer case => 0
 Instead of 1
 '''
+# Type Casting - Converting one data type into another
 # Access a index position in list
-students =  ["Rohan", "Subham" , "Aditya", 'John', "Rutuja" ] # List Variable
-students[2] = "Manoj"
-print(students)
+# students =  ["Rohan", "Subham" , "Aditya", 'John', "Rutuja" ] # List Variable
+# students[2] = "Manoj"
+# print(students)
+# userInput = int(input())
+# board[userInput - 1] = "X"
 
+
+'''Tic Tac Toe'''
 # Design the board
 board = [
         "-", "-", "-",
         "-", "-", "-",
         "-", "-", "-"
         ]
-# userInput = int(input())
-# board[userInput - 1] = "X"
 
 has_won = None  # To store the winner name
 
+game_is_still_going = True  # To check if the game is still going on
+
+current_player = "X"  #  To store the current player
+  
 # Display the board with updated user input
 def display_board():
     print("\n")
@@ -40,25 +47,66 @@ def display_board():
     print("\n")
 
 # Check if current user has valid input or not 
-def handle_turn():
-    return
+def handle_turn(player):
+
+    print(player + "'s turn")
+    position = input("Choose a position between (1-9): ")
+
+    # Check if input is valid
+    valid = False
+    while not valid:
+
+        # Check if position is between 1 - 9
+        while position not in ["1", "2", "3", "4", "5", "6", "7", "8", "9"]:
+            position = input("Not valid postion! Please choose another position between (1-9): ")
+        
+        position = int(position) - 1
+
+        # Check whether there is some input in that position or not
+        if(board[position] == "-"):
+            valid = True
+        else:
+            print("You can't overwrite a position!")
+
+    board[position] = player
+    display_board()
 
 # Check if game over
 def check_if_game_over():
+    check_if_win()
+    check_if_tie()
+
+# Check if there is tie
+def check_if_tie():
+    return
+
+# Check if someone won
+def check_if_win():
     return
 
 # Flip the current player to next player
 def flip_player():
-    return
+    # Call Global variable
+    global current_player
+
+    if current_player == "X":
+        current_player = "O"
+    elif current_player == "O":
+        current_player = "X"
 
 # Play the game
 def play_game():
     display_board()
-    handle_turn()
-    check_if_game_over()
-    flip_player()
+    
+    # To check if the game is still running
+    while game_is_still_going:
+        handle_turn(current_player)
+        check_if_game_over()
+        flip_player()
 
     if has_won == "X" or has_won == "O":
         print("Our winner is "+has_won)
     elif has_won == None:
         print("Game Tie")
+
+play_game()
